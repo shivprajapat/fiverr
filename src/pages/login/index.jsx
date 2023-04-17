8fe9a5198ba7b4d594f63cd88f0244adf5c0c340
yudiz-shiv <shivkumar.prajapat@yudiz.com>
2023-04-17 09:51:34 +0530

    feat: 🎸 login api use

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -13,7 +13,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://fiverr-backend-9nqc.onrender.com/api/auth/login", {
         username,
         password,
       });
